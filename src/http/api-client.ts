5f4a57ec4701e119c09730931b83c5bca143214d
Cesar <cesarscfilho@gmail.com>
2025-07-18 15:21:32 -0300

    refactor: ky -> axios

--- src/http/api-client.ts
+++ src/http/api-client.ts
@@ -1,23 +1,16 @@
-import axios, { type InternalAxiosRequestConfig } from "axios"
-import { type CookiesFn, getCookie } from "cookies-next"
+import axios from "axios"
+import { getCookie } from "cookies-next"
 import { env } from "@/env"
 
 const api = axios.create({
   baseURL: env.NEXT_PUBLIC_API_URL,
 })
 
-api.interceptors.request.use(async (config: InternalAxiosRequestConfig) => {
-  let cookieStore: CookiesFn | undefined
+api.interceptors.request.use(config => {
+  const token = getCookie("token")
 
-  if (typeof window === "undefined") {
-    const { cookies: serverCookies } = await import("next/headers")
-    cookieStore = serverCookies
-  }
-
-  const token = await getCookie("token", { cookies: cookieStore })
-
-  if (token && config.headers?.set) {
-    config.headers.set("Authorization", `Bearer ${token}`)
+  if (token && config.headers) {
+    config.headers.Authorization = `Bearer ${token}`
   }
 
   return config
--- src/http/get-partner.ts
+++ src/http/get-partner.ts
@@ -1,8 +1,45 @@
-import type { Partner } from "@/lib/validations/partner"
+import { AxiosError } from "axios"
 import { api } from "./api-client"
 
-export async function getPartner() {
-  const result = await api.get("partner").json<Partner>()
+export type Partner = {
+  id: string
+  name: string
+  email: string
+  establishments: {
+    id: string
+    name: string
+  }[]
+  subscriptions: {
+    id: string
+    status: string
+    endedAt: Date
+    createdAt: Date
+  }[]
+}
 
-  return result
+interface GetPartnerResponse {
+  partner: Partner
+}
+
+export async function getPartner() {
+  try {
+    const response = await api.get<GetPartnerResponse>("/partner")
+
+    return {
+      data: response.data,
+      error: null,
+    }
+  } catch (err) {
+    if (err instanceof AxiosError) {
+      return {
+        data: null,
+        error: err.message,
+      }
+    }
+
+    return {
+      data: null,
+      error: "Erro inesperado, tente novamente em alguns minutos.",
+    }
+  }
 }
